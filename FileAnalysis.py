c455dfc491dafdc6fc729943b7e1f6c95d0df401
enricoguerra96 <guerra1996@libero.it>
2021-04-22 17:35:29 +0200

    Modificato progetto

--- FileAnalysis.py
+++ FileAnalysis.py
@@ -13,45 +13,72 @@ from translate import Translator
 nlp = spacy.load("it_core_news_lg")
 
 
+def read_from(filepath: str):
+    # Reads the content of the given file, returns the corresponding string
+    content = ""
+    try:
+        with open(filepath, 'r') as r:
+            content = r.read()
+            content = " ".join(content.split())
+    except FileNotFoundError:
+        print("File {} not found.".format(filepath))
+        exit()
+    return content
+
+
 def text_translation(text: str):
     translator = Translator(to_lang="Italian")
     translation = translator.translate(text)
     return translation
 
 
-def clean_dir(dir_path):
+def clean_dir(dir_path: str):
     shutil.rmtree(dir_path)
     os.mkdir(dir_path)
 
 
-def count_frequencies(input_text: list):
-    # Create BoW
+def create_bow(input_text: list):
+    # Create BoW of the given news without any cut
     frequency = {}
     for item in input_text:
         if item in frequency:
             frequency[item] += 1
         else:
             frequency[item] = 1
+    return frequency
 
+
+def bow_to_str(input_bow: dict):
+    out = ""
+    for key, value in input_bow.items():
+        for _ in range(value):
+            out += str(key) + " "
+    return out
+
+
+def count_frequencies(input_text: list):
+    # Create BoW
+    frequency = create_bow(input_text)
     correct_frequency = {}
 
-    # Eliminate less frequency words
+    # Eliminate low frequency words
     for key, value in frequency.items():
         if value > 1:
             correct_frequency[key] = value
 
-    # Re-create string, cut words to 30
+    # Re-create string, cut words up to TF = 30
     correct_str = ""
     limit = 0
     for key, value in correct_frequency.items():
         if limit < 30:
-            for i in range(value):
+            for _ in range(value):
                 correct_str += str(key) + " "
         limit += 1
     return correct_str
 
 
 def is_correct(input_text: str, lang: str):
+    # Check grammar errors removing well-known words
     try:
         tool = language_tool_python.LanguageTool(lang)
         matches = tool.check(input_text)
@@ -72,7 +99,8 @@ def text_analysis(input_text: str):
     # Tokenization
     text_toked = word_tokenize(input_text)
 
-    # Stopwords/punctuation elimination
+    # Stopwords/punctuation/words with len(1)/frequent words elimination
+
     stop_words = set(stopwords.words('italian'))
     filtered_text = []
 
@@ -97,7 +125,7 @@ def count_words(input_text: str):
 
 
 def bufale_scrap_download(url: str, index: str):
-    # save as local HTML (Indici)
+    # save as local HTML (directory: Indici)
     time_obj = datetime.now()
     filename = "./BufaleNet/Indici/" + str(index) + "_" + str(time_obj) + ".html"
     urllib.request.urlretrieve(url, filename)
@@ -112,16 +140,14 @@ def bufale_scrap_download(url: str, index: str):
     for element in soup.select('a'):
         href = element.get('href')
         if str(href) is not None and str(href) != '' and '-' in str(href):
-            text = element.text
-            if str(text) is not None and str(text) != '' and str(text).find('\n'):
-                if count_words(str(text)) > 3:
-                    page = requests.get(str(href))
-                    text_soup = BeautifulSoup(page.text, "lxml")
+            if str(element.text) is not None and str(element.text) != '' and str(element.text).find('\n'):
+                if count_words(str(element.text)) > 3:
+                    text_soup = BeautifulSoup(requests.get(str(href)).text, "lxml")
                     for t in text_soup.find_all(class_="text-article"):
-                        in_links.append(str(text + " " + t.text))
+                        in_links.append(str(element.text + " " + t.text))
                         in_hrefs.append(str(href))
 
-    # Divide all in files and save (Pagine)
+    # Divide all in files and save from in_links (directory: Pagine)
     ind = 0
     for text in in_links:
         if ind != len(in_links):
@@ -159,8 +185,7 @@ def butac_scrap_download(url: str, index: str):
         href = element.get('href')
         if str(href) is not None and str(href) != '' and str(href) not in whocares_hrefs \
                 and 'https://www.butac.it/' in str(href) and '-' in str(href):
-            text = element.text
-            in_texts_href.append(text)
+            in_texts_href.append(element.text)
             in_hrefs.append(str(href))
 
     # Remove duplicate hrefs
@@ -176,7 +201,7 @@ def butac_scrap_download(url: str, index: str):
             temp += t.text
         in_links.append(temp)
 
-    # Divide in files and save
+    # Divide in files and save (directory: Pagine)
     for ind in range(len(in_links)):
         new_href = in_hrefs[ind].replace('https://www.butac.it/', '')[:-1]
         filename = "./Butac/Pagine/" + new_href + ".txt"
@@ -185,17 +210,19 @@ def butac_scrap_download(url: str, index: str):
 
 
 def butac_checkupdates():
+    # Refresh scraped pages
     basic_url = "https://www.butac.it/bufala/page/"
-    # butac_scrap_download("https://www.butac.it/bufala", str(1))
-    for i in range(1, 20):
+    butac_scrap_download("https://www.butac.it/bufala", str(1))
+    for i in range(1, 10):
         new_url = basic_url + str(i)
         butac_scrap_download(new_url, str(i))
 
 
 def bufale_checkupdates():
+    # Refresh scraped pages
     basic_url = "https://www.bufale.net/bufala/page/"
-    # bufale_scrap_download("https://www.bufale.net/bufala", str(1))
-    for i in range(10, 20):
+    bufale_scrap_download("https://www.bufale.net/bufala", str(1))
+    for i in range(1, 10):
         new_url = basic_url + str(i)
         bufale_scrap_download(new_url, str(i))
 
@@ -209,12 +236,16 @@ def simil_spacy(input1: str, input2: str):
 def news_control(news: str, site: str):
     # Retrieve the maximum similarity index for each news in the directory
     try:
-        input1_string = count_frequencies(text_analysis(news))
+        input1_string = bow_to_str(create_bow(text_analysis(news)))
+        if len(input1_string.strip()) == 0:
+            return print("The input news is too short. Please insert a longer text.")
         directory = "./" + site + "/Pagine/"
         simil_records, simil_texts = [], []
         for filename in os.listdir(directory):
             with open(directory + filename, 'r') as f:
                 input2 = f.read()
+                if os.stat(directory + filename).st_size == 0:
+                    return print(filename + " :  file is empty")
                 simil_records.append(simil_spacy(input1_string, input2))
                 simil_texts.append(str(filename))
 
@@ -227,26 +258,26 @@ def news_control(news: str, site: str):
             simil_records.remove(m)
 
     except FileNotFoundError:
-        print("No directory with this name.")
+        print("No directories with this name.")
     except ValueError:
         print("Maybe there are no pages in this directory.")
 
 
-def analyze_news():
-    print("Insert text:\n")
-    text = input()
-    print("\n")
+def analyze_news(filepath: str):
+    print("Beginning")
+    text = read_from(filepath)
+    if len(text.strip()) == 0:
+        return print("The input news can't be empty.")
     is_correct(text, "it")
-    print("\n Similarities:")
+    print("\nSimilarities:")
     news_control(text, "BufaleNet")
     news_control(text, "Butac")
 
 
-clean_dir("./BufaleNet/Indici/")
-clean_dir("./BufaleNet/Pagine/")
-clean_dir("./Butac/Indici/")
-clean_dir("./Butac/Pagine/")
-bufale_checkupdates()
-butac_checkupdates()
-analyze_news()
-
+# clean_dir("./BufaleNet/Indici/")
+# clean_dir("./BufaleNet/Pagine/")
+# clean_dir("./Butac/Indici/")
+# clean_dir("./Butac/Pagine/")
+# bufale_checkupdates()
+# butac_checkupdates()
+analyze_news("./my_news")
